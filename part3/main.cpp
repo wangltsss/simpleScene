51a41c55203d67cb105aa07b66fac68493788cd5
Shentong Wang <wangs396@mcmaster.ca>
2022-11-21 18:00:19 -0500

    Part 1 finished.

---
+++ part3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  part3
+//
+//  Created by 王沈同 on 2022-11-20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
